393e120f0efc8b4bc016bfc9583c89bf0fab29e2
Muhamad Arga Agustiar <argaagustiar@gmail.com>
2024-02-24 16:00:01 +0700

    Initial commit

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,6 @@
+<a href="/">
+    <svg class="w-16 h-16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <image href="{{ asset('images/red-shopping-bag.png') }}" width="48" height="48"/>
+    </svg>
+</a>
+    
